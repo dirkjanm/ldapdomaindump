918ec5575344ad566bff466f4811938b9d7eae9d
iotmani <salism3@gmail.com>
2019-01-17 15:42:45 -0500

    Update __init__.py

--- ldapdomaindump/__init__.py
+++ ldapdomaindump/__init__.py
@@ -454,10 +454,10 @@ class reportWriter(object):
     #Parse bitwise flags into a list
     def parseFlags(self, attr, flags_def):
         outflags = []
-        if attr is None:
+        if attr is None or attr.value is None:
             return outflags
         for flag, val in iteritems(flags_def):
-            if attr.value != None and attr.value & val:
+            if attr.value & val:
                 outflags.append(flag)
         return outflags
 
