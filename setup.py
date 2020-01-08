7a59286b6f201373fcc224d623bacad7552d9841
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2020-01-08 15:57:07 +0100

    ldap3 version upped, fixes #19 and fixes #20

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(name='ldapdomaindump',
-      version='0.9.1',
+      version='0.9.2',
       description='Active Directory information dumper via LDAP',
       author='Dirk-jan Mollema',
       author_email='dirkjan@sanoweb.nl',
       url='https://github.com/dirkjanm/ldapdomaindump/',
       packages=['ldapdomaindump'],
-      install_requires=['dnspython', 'ldap3>=2.6.1', 'future'],
+      install_requires=['dnspython', 'ldap3>=2.5,!=2.5.2,!=2.5.0,!=2.6', 'future'],
       package_data={'ldapdomaindump': ['style.css']},
       include_package_data=True,
       scripts=['bin/ldapdomaindump', 'bin/ldd2bloodhound']
